b8a6ae9061ba863c5c55778cdb907aae404de0bb
Yuki-Uekihara <yuki.uekihara@mail.o-hara.ac.jp>
2025-09-04 15:12:00 +0900

    Initial commit

---
+++ Src/Main.cpp
@@ -0,0 +1,113 @@
+/*
+ *	@file	Main.cpp
+ */
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Windows.h>
+#include <string>
+
+/*
+ *	エントリーポイント
+ */
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+) {
+
+	//	GLFWの初期化
+	if (glfwInit() != GLFW_TRUE)
+		return 1;	//	失敗
+
+	//	描画ウィンドウの作成
+	GLFWwindow* window = nullptr;				//	ウィンドウオブジェクト
+	const std::string title = "OpenGLGame";		//	ウィンドウのタイトル
+	window = glfwCreateWindow(1280, 720, title.c_str(), nullptr, nullptr);
+
+	if (!window) {
+		glfwTerminate();
+		return 1;	//	失敗
+	}
+
+	//	OpenGLコンテキストの作成
+	glfwMakeContextCurrent(window);
+
+	//	OpenGL関数のアドレスを取得
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+		glfwTerminate();
+		return 1;	//	失敗
+	}
+
+	//	頂点データ
+	const float vertexData[][3] = {
+		{ -0.2f, -0.2f, 0.0f },
+		{  0.2f, -0.2f, 0.0f },
+		{  0.0f,  0.2f, 0.0f },
+
+		{ -0.8f, -0.2f, 0.0f },
+		{ -0.4f, -0.2f, 0.0f },
+		{ -0.4f,  0.2f, 0.0f },
+		{ -0.8f,  0.2f, 0.0f },
+	};
+
+	//	頂点バッファの管理番号
+	GLuint vbo = 0;		//	vertex buffer object
+	//	バッファの作成
+	glCreateBuffers(1, &vbo);
+	//	GPUメモリを確保しデータをコピーする
+	glNamedBufferStorage(vbo, sizeof(vertexData), vertexData, 0);
+
+
+	//	インデックスデータ
+	const GLushort indexData[] = {
+		0, 1, 2,
+		3, 4, 5,  5, 6, 3,
+	};
+
+	//	インデックスバッファの管理番号
+	GLuint ibo = 0;		//	index buffer object
+	//	バッファの作成
+	glCreateBuffers(1, &ibo);
+	//	GPUメモリを確保しデータをコピーする
+	glNamedBufferStorage(ibo, sizeof(indexData), indexData, 0);
+
+
+	//	頂点属性(Vertex Attribute)配列の管理番号
+	GLuint vao = 0;		//	vertex array object
+	//	VAO を生成
+	glCreateVertexArrays(1, &vao);
+	//	OpenGLコンテキスト にバインド
+	glBindVertexArray(vao);
+
+	//	IBO を OpenGLコンテキスト と VAO にバインド
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	//	VBO を OpenGLコンテキスト にバインド
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//	0番目の頂点属性(Vertex Attribute)を有効化
+	glEnableVertexAttribArray(0);
+	//	0番目の頂点属性を設定
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+
+	//	メインループ
+	while (!glfwWindowShouldClose(window)) {
+		//	バックバッファをクリア
+		glClearColor(0.9f, 0.6f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//	図形を描画
+		glBindVertexArray(vao);
+		glDrawElementsInstanced(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, 0, 1);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//	GLFWの終了
+	glfwTerminate();
+
+	return 0;
+}
